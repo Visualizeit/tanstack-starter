1941273b179ef2d7e9cd690f2adc407a4f3b0863
Visualizeit <17688172112@163.com>
2024-10-29 14:33:12 +0800

    Refactor __root.tsx to remove TanStackRouterDevtools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,17 +1,11 @@
 import { type QueryClient } from '@tanstack/react-query'
 import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 interface RouterContext {
 	queryClient: QueryClient
 }
 
-const AppRoot = () => (
-	<>
-		<Outlet />
-		<TanStackRouterDevtools />
-	</>
-)
+const AppRoot = () => <Outlet />
 
 export const Route = createRootRouteWithContext<RouterContext>()({
 	component: AppRoot,
