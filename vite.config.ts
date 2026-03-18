988bcc7a62db1e9a41812023afc569cdbaf2d0dd
Visualizeit <17688172112@163.com>
2026-03-18 22:03:03 +0800

    Upgrade Vite stack and simplify tsconfig path resolution

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,12 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import { nitro } from 'nitro/vite'
 import { defineConfig } from 'vite'
-import devtoolsJson from 'vite-plugin-devtools-json'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 import customScaffolding from './custom-scaffolding'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        devtoolsJson(),
-        tsconfigPaths(),
         tailwindcss(),
         tanstackStart({
             router: {
@@ -22,6 +18,9 @@ export default defineConfig({
         nitro(),
         viteReact(),
     ],
+    resolve: {
+        tsconfigPaths: true,
+    },
     server: {
         open: true,
     },
