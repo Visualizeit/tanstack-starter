dae979f264c98d2d571205581994d90ace8cccad
Visualizeit <17688172112@163.com>
2026-06-27 20:30:15 +0800

    refactor: remove ignorePatterns and unnecessary overrides from configuration files

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -3,7 +3,6 @@ import ultracite from 'ultracite/oxfmt'
 
 export default defineConfig({
     ...ultracite,
-    ignorePatterns: [...(ultracite.ignorePatterns as string[]), '.agents/**'],
     semi: false,
     singleQuote: true,
     tabWidth: 4,
