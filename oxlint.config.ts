85b194fe619d65e3e54761edcc137ff4f8738c5e
Visualizeit <17688172112@163.com>
2026-06-29 21:02:11 +0800

    refactor: update dependencies to version 9.4.1 and add tanstack to oxlint configuration

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'oxlint'
 import core from 'ultracite/oxlint/core'
 import react from 'ultracite/oxlint/react'
+import tanstack from 'ultracite/oxlint/tanstack'
 
 export default defineConfig({
-    extends: [core, react],
+    extends: [core, react, tanstack],
+    ignorePatterns: core.ignorePatterns,
     rules: {
         'promise/prefer-await-to-then': 'off',
     },
